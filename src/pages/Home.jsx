b0d24a4a181489b46d9506abaf8405ac8c2d1019
HadziqAlaydrus <hadziq.alaydrus361@gmail.com>
2024-05-18 19:59:32 +0700

    Homepage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -15,7 +15,7 @@ function Home() {
         <p class="mb-6 font-normal text-white dark:text-gray-400">Website pembayaran sampah dengan fitur memberikan tugas sesuai permintaan klien dan fitur pengambilan sampah sesuai permintaan dapat menjadi alat yang sangat berguna dalam upaya mengelola limbah dengan lebih efisien dan efektif. Jadi tunggu apa lagi, gabung jadi mitra kami sekarang</p>
         </a>
         <a href="/login" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-black bg-white rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-white dark:bg-white dark:hover:bg-white dark:focus:ring-white">
-          Read more
+          Daftar Sekarang
           <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
               <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
           </svg>
