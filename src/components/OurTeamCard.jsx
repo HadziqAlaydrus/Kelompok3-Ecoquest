b2bc520c720ad814aa67619c5bc7663c619fa6c6
hadziqalaydrus <hadziq.alaydrus361@gmail.com>
2024-05-21 21:50:11 +0700

    NewUpdate

--- src/components/OurTeamCard.jsx
+++ src/components/OurTeamCard.jsx
@@ -1,59 +1,99 @@
 import React from "react";
+import react, { useEffect } from 'react';
 import logo from "../assets/logo.jpeg";
 import hadziq from  "../assets/hadziq2.jpg"
 import { FaGithubAlt } from "react-icons/fa6";
 import { FaLinkedin } from "react-icons/fa";
 import { RiInstagramFill, RiLandscapeFill } from "react-icons/ri";
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+
 
 const team = [
   {
-    img: logo,
+    img: hadziq,
     name: "Suci Fitrianti",
     role: "Leader",
+    github: "",
+    linkedin: "",
+    instagram:"",
+
   },
   {
     img: hadziq,
-    name: "Abubakar Hadziq",
-    role: "Developer",
+    name: "Abubakar Hadziq Alaydrus",
+    role: "Member",
+    github: "https://github.com/HadziqAlaydrus",
+    linkedin: "https://www.linkedin.com/in/abubakar-hadziq-alaydrus-a3b24722b/",
+    instagram:"https://www.instagram.com/hadziqalaydrus/",
+
   },
   {
-    img: logo,
+    img: hadziq,
     name: "Anang Arif Hibaturrahman",
-    role: "Developer",
+    role: "Member",
+    github: "",
+    linkedin: "",
+    instagram:"",
+
   },
   {
-    img: logo,
+    img: hadziq,
     name: "Muhammad Chair Rizieq",
-    role: "Developer",
+    role: "Member",
+    github: "",
+    linkedin: "",
+    instagram:"https://www.instagram.com/rziieqm_",
+
   },
   {
-    img: logo,
-    name:"Raihan Aji",
-    role:"Developer",
+    img: hadziq,
+    name:"Raihan Aji Putra Darmawan",
+    role:"Member",
+    github: "",
+    linkedin: "",
+    instagram:"",
+
   },
   {
-    img: logo,
-    name:"Ibnu Adeng",
-    role:"Developer",
+    img: hadziq,
+    name:"Ibnu Adeng Kurnia",
+    role:"Member",
+    github: "https://github.com/ibnuadeng03",
+    linkedin: "http://www.linkedin.com/in/ibnu-adeng-kurnia-351648301",
+    instagram:"https://www.instagram.com/ibnuadeng0303/",
+
   },
   {
-    img:logo,
+    img:hadziq,
     name:"Novian Pratama",
-    role: "Developer",
+    role: "Member",
+    github: "",
+    linkedin: "",
+    instagram:"",
+
   }
 ];
 
 const OurTeamCard = () => {
+  useEffect(() => {
+  AOS.init({
+    duration: 1000,
+    easing: 'ease-in-out',
+  })
+  }, []);
+
   return (
     <section className=' bg-transparant h-full'>
-      <h1 className='text-white font-bold text-[30px] text-center py-24'>
+      <h1 className='text-white font-bold text-[30px] text-center py-36'>
         Our Team
       </h1>
-      <div className='grid md:grid-cols-2 xl:grid-cols-4  justify-center items-center  py-24'>
+      <div className='grid md:grid-cols-2 xl:grid-cols-4  justify-center items-center  py-24='>
         {team.map((profile, index) => (
           <div
             key={index}
             className='bg-gray-900 w-[300px] mb-36 mx-auto  p-3 rounded-xl border-2 border-[#97EB63] '
+            data-aos='fade-up'
           >
             <img
               className='mx-auto -mt-32 rounded-full w-[150px] h-[150px] md:w-[200px] md:h-[200px] object-cover'
@@ -68,21 +108,27 @@ const OurTeamCard = () => {
               {profile.role}
             </p>
             <div className='flex justify-center items-center gap-5 py-2'>
+              <a href={profile.github} target="_blank" rel="noopener noreferrer">
               <FaGithubAlt
-                href='https://github.com/HadziqAlaydrus'
+                href='/'
                 fill='#97EB63'
                 className='w-5 h-5 hover:scale-110 transition-all cursor-pointer'
               />
+              </a>
+              <a href={profile.linkedin}target="_blank" rel="noopener noreferrer">
               <FaLinkedin
                 href='/'
                 fill='#97EB63'
                 className='w-5 h-5 hover:scale-110 transition-all cursor-pointer'
               />
+              </a>
+              <a href={profile.instagram} target="_blank" rel="noopener noreferrer">
               <RiInstagramFill
                 href='/'
                 fill='#97EB63'
                 className='w-5 h-5 hover:scale-110 transition-all cursor-pointer'
               />
+              </a>
             </div>
           </div>
           
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -6,7 +6,7 @@ function About() {
   return (
     <div className="max-w-screen p-14 bg-gradient-to-tl from-cyan-800 via-teal-500 to-lime-500 pt-36">
       <div className="flex text-justify justify-between gap-8">
-        <div className="bg-white flex flex-col justify-between gap-y-16 px-32 py-12 border border-red-300 rounded-3xl text-center">
+        <div className="bg-white flex flex-col justify-between gap-y-16 px-32 py-20  border border-red-300 rounded-3xl text-center">
           <img src={ablogo} alt="" />
           <h2 className="mt-5 mx-auto text-justify">
             Environmental <br></br>Awareness  for<br /> The Future
@@ -14,7 +14,7 @@ function About() {
         </div>
         <div>
           <h1 className="text-white pt-32 pr-20 text-7xl">What Is EcoQuest?</h1>
-          <h3 className="text-white pt-5 pr-20">
+          <h3 className="text-white pt-5 pr-20 text-xl">
             Website pembayaran sampah dengan fitur memberikan tugas sesuai
             permintaan klien dan fitur pengambilan sampah sesuai permintaan
             dapat menjadi alat yang sangat berguna dalam upaya mengelola limbah
