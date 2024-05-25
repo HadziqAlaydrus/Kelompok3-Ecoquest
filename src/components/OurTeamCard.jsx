caf1ad485943cdd8a4e41a50876603bbcd082b09
hadziqalaydrus <hadziq.alaydrus361@gmail.com>
2024-05-25 16:14:30 +0700

    new update

--- src/components/OurTeamCard.jsx
+++ src/components/OurTeamCard.jsx
@@ -14,9 +14,9 @@ const team = [
     img: hadziq,
     name: "Suci Fitrianti",
     role: "Leader",
-    github: "",
-    linkedin: "",
-    instagram:"",
+    github: "https://github.com/Sucifitrianti",
+    linkedin: "https://www.linkedin.com/in/suci-fitrianti-66b6272bb",
+    instagram:"https://www.instagram.com/sciftrianti02?igsh=aWU4M3N6OXg3Nnh6",
 
   },
   {
@@ -32,9 +32,9 @@ const team = [
     img: hadziq,
     name: "Anang Arif Hibaturrahman",
     role: "Member",
-    github: "",
-    linkedin: "",
-    instagram:"",
+    github: "https://github.com/anangarif09",
+    linkedin: "http://www.linkedin.com/in/anang-arif-hibaturrahman-b468931a6",
+    instagram:"https://www.instagram.com/anangarif_h?igsh=MXBrN3hwY2F4OGgzcw==",
 
   },
   {
@@ -51,8 +51,8 @@ const team = [
     name:"Raihan Aji Putra Darmawan",
     role:"Member",
     github: "",
-    linkedin: "",
-    instagram:"",
+    linkedin: "http://linkedin.com/in/raihanajip-darmawan/",
+    instagram:"https://www.instagram.com/hi_banghan/",
 
   },
   {
@@ -69,8 +69,8 @@ const team = [
     name:"Novian Pratama",
     role: "Member",
     github: "",
-    linkedin: "",
-    instagram:"",
+    linkedin: "https://www.linkedin.com/in/nofian-pratama-33827a2a5?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
+    instagram:"https://www.instagram.com/nofian.prtm",
 
   }
 ];
@@ -84,15 +84,15 @@ const OurTeamCard = () => {
   }, []);
 
   return (
-    <section className=' bg-transparant h-full'>
+    <section className='bg-transparent h-full'>
       <h1 className='text-white font-bold text-[30px] text-center py-36'>
         Our Team
       </h1>
-      <div className='grid md:grid-cols-2 xl:grid-cols-4  justify-center items-center  py-24='>
-        {team.map((profile, index) => (
+      <div className='grid md:grid-cols-2 xl:grid-cols-4 justify-center items-center py-24'>
+        {team.slice(0, 4).map((profile, index) => (
           <div
             key={index}
-            className='bg-gray-900 w-[300px] mb-36 mx-auto  p-3 rounded-xl border-2 border-[#97EB63] '
+            className='bg-gray-900 w-[300px] mb-36 mx-auto p-3 rounded-xl border-2 border-[#97EB63]'
             data-aos='fade-up'
           >
             <img
@@ -108,31 +108,71 @@ const OurTeamCard = () => {
               {profile.role}
             </p>
             <div className='flex justify-center items-center gap-5 py-2'>
-              <a href={profile.github} target="_blank" rel="noopener noreferrer">
-              <FaGithubAlt
-                href='/'
-                fill='#97EB63'
-                className='w-5 h-5 hover:scale-110 transition-all cursor-pointer'
-              />
+              <a href={profile.github} target='_blank' rel='noopener noreferrer'>
+                <FaGithubAlt
+                  fill='#97EB63'
+                  className='w-5 h-5 hover:scale-110 transition-all cursor-pointer'
+                />
               </a>
-              <a href={profile.linkedin}target="_blank" rel="noopener noreferrer">
-              <FaLinkedin
-                href='/'
-                fill='#97EB63'
-                className='w-5 h-5 hover:scale-110 transition-all cursor-pointer'
-              />
+              <a href={profile.linkedin} target='_blank' rel='noopener noreferrer'>
+                <FaLinkedin
+                  fill='#97EB63'
+                  className='w-5 h-5 hover:scale-110 transition-all cursor-pointer'
+                />
               </a>
-              <a href={profile.instagram} target="_blank" rel="noopener noreferrer">
-              <RiInstagramFill
-                href='/'
-                fill='#97EB63'
-                className='w-5 h-5 hover:scale-110 transition-all cursor-pointer'
-              />
+              <a href={profile.instagram} target='_blank' rel='noopener noreferrer'>
+                <RiInstagramFill
+                  fill='#97EB63'
+                  className='w-5 h-5 hover:scale-110 transition-all cursor-pointer'
+                />
               </a>
             </div>
           </div>
-          
         ))}
+        <div className='col-span-4 flex justify-center'>
+          <div className='flex justify-center gap-4 mt-5'>
+            {team.slice(4).map((profile, index) => (
+              <div
+                key={index + 4}
+                className='bg-gray-900 w-[300px] mb-36 p-3 rounded-xl border-2 border-[#97EB63]'
+                data-aos='fade-up'
+              >
+                <img
+                  className='mx-auto -mt-32 rounded-full w-[150px] h-[150px] md:w-[200px] md:h-[200px] object-cover'
+                  src={profile.img}
+                  alt=''
+                />
+                <h3 className='text-center text-white font-bold text-[20px] md:mt-5'>
+                  {profile.name}
+                </h3>
+                <div className='w-full bg-[#97EB63] h-0.5'></div>
+                <p className='text-center text-[#97EB63] font-light text-[14px]'>
+                  {profile.role}
+                </p>
+                <div className='flex justify-center items-center gap-5 py-2'>
+                  <a href={profile.github} target='_blank' rel='noopener noreferrer'>
+                    <FaGithubAlt
+                      fill='#97EB63'
+                      className='w-5 h-5 hover:scale-110 transition-all cursor-pointer'
+                    />
+                  </a>
+                  <a href={profile.linkedin} target='_blank' rel='noopener noreferrer'>
+                    <FaLinkedin
+                      fill='#97EB63'
+                      className='w-5 h-5 hover:scale-110 transition-all cursor-pointer'
+                    />
+                  </a>
+                  <a href={profile.instagram} target='_blank' rel='noopener noreferrer'>
+                    <RiInstagramFill
+                      fill='#97EB63'
+                      className='w-5 h-5 hover:scale-110 transition-all cursor-pointer'
+                    />
+                  </a>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
       </div>
     </section>
   );
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -3,6 +3,14 @@ import React from 'react'
 function Contact() {
   return (
     <div className='max-w-screen p-14 bg-gradient-to-tl from-cyan-800 via-teal-500 to-lime-500 pt-36'>
+      <div className='bg-white p-14'>
+        <h1>Contact</h1>
+        <ul>
+          <li>Jl. rumah jikku </li>
+          <li>Telp 0894859 </li>
+          <li>ecoquest@gmail.com </li>
+        </ul>
+      </div>
       
     </div>
   )
